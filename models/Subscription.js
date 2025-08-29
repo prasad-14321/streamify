3d228ad79471815c3efd99123676cc64ed4f558b
prasad-14321 <prasadteeti9@gmail.com>
2025-08-29 09:46:59 +0530

    Delete models directory

--- models/Subscription.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const subscriptionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  plan: { type: String, enum: ['Basic', 'Premium'], default: 'Basic' },
-  validTill: Date
-});
-
-module.exports = mongoose.model('Subscription', subscriptionSchema);
