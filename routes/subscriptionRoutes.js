3dd9110d9c704d50f6c462b17e639fccc78cce37
prasad-14321 <prasadteeti9@gmail.com>
2025-08-29 09:47:23 +0530

    Delete routes directory

--- routes/subscriptionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { subscribeUser } = require('../controllers/subscriptionController');
-const auth = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/subscribe', auth, subscribeUser);
-
-module.exports = router;
